6cd0f00e1349cdc114c96e8b4dee255aeee8ee7e
M.Andhika <48423397+andhika276@users.noreply.github.com>
2021-05-07 14:28:53 +0700

    update

---
+++ resources/migrations/20210507062515-membuat-kategori_nilai-nilai.cjs
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Kategori_nilai', {
+      parent: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        primaryKey: id_kategori,
+      },
+      nama_kategori: {
+        type: Sequelize.STRING,
+      },
+      id_mata_kuliah: {
+        type: Sequelize.STRING,
+        references:{
+          model:'Mata_kuliah',
+          key:'id_matakuliah',
+        },
+        allowNull: false,
+      },
+      bobot_nilai: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+    await queryInterface.createTable('Nilai', {
+      id_nilai: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+          model:"Mahasiswa",
+          key:"id_nilai",
+        },
+        primaryKey: true
+      },
+      id_kategori: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:'Kategori_nilai',
+          key:'id_kategori',
+        }
+      },
+      nilai: {
+        type: Sequelize.INTEGER
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Kategori_nilai');
+    await queryInterface.dropTable('Nilai');
+  }
+};
