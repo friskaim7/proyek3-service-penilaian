5cfec3d499a6db997c176d9f08770ba50ef26c46
M.Andhika <48423397+andhika276@users.noreply.github.com>
2021-06-10 23:56:00 +0700

    Update fix get mata kuliah and get kelas by nip

---
+++ src/controller/Kelas.js
@@ -0,0 +1,40 @@
+import * as PengajarDAO from '../dao/Pengajar'
+import * as PerkuliahaDAO from '../dao/Perkuliahan'
+import * as KelasDAO from '../dao/Kelas'
+import { validationResult } from 'express-validator/check'
+
+export const getKelasAjarByDosen = async (req, res) => {
+    try {
+        const nip = req.params.nip
+        const pengajar = await PengajarDAO.findPengajarByNIP(nip)
+        var i
+        var j
+        var listIdPerkuliahan = []
+        var listKelas = []
+        for (i = 0; i < pengajar.length; i++){
+            var idPerkuliahan = pengajar[i].id_perkuliahan
+            listIdPerkuliahan.push(idPerkuliahan)
+        }
+        for (i = 0; i < listIdPerkuliahan.length; i++){
+            var perkuliahan = await PerkuliahaDAO.findPerkuliahanById(listIdPerkuliahan[i])
+            var kelas = await KelasDAO.findKelasByKodeKelas(perkuliahan.kode_kelas)
+            listKelas.push(kelas)
+        }
+        for(i=0; i < listKelas.length; i++){
+            for (j=i+1; j <listKelas.length; j++){
+                if (listKelas[i].kode_kelas == listKelas[j].kode_kelas){
+                    listKelas.splice(j, 1)
+                }
+            }
+        }
+        res.status(200).json({
+            message: 'get matkul by dosen sukses',
+            data: {
+                listKelas
+            }
+        })
+    }
+    catch (error) {
+        res.status(error.status).json({ error })
+    }
+}
--- src/routes/Dosen.js
+++ src/routes/Dosen.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import * as DosenController from '../controller/Dosen'
+import * as MatkulController from '../controller/Mata Kuliah'
+import * as KelasController from '../controller/Kelas'
 import * as ValidatorSanitizer from '../middleware/InputValidatorSanitizer'
 
 const router = express.Router()
 
 router.get('/get-one/:NIP', DosenController.getDosenByNIP)
 router.get('/get-perkuliahan-dosen/:nip', DosenController.getPerkuliahanDosen)
-router.get('/get-kelas-dosen/:nip', DosenController.getKelasDosen)
-router.get('/get-matakuliah-dosen/:nip', DosenController.getMataKuliahDosen)
+router.get('/dosen/matkul/:nip/:id_kelas', MatkulController.getMatkulAjarByDosen)
+router.get('/dosen/kelas/:nip', KelasController.getKelasAjarByDosen)
 router.get('/all-dosen', DosenController.getAllDosen)
-router.get('', DosenController.getDosenByJabatan)
 router.post('/new-dosen', ValidatorSanitizer.postNewDosen, DosenController.postNewDosen)
 router.delete('/delete/:NIP', DosenController.deleteDosenByNIP)
 
