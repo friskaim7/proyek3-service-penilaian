c27fa895070fa17fd9824650b87771460a02acde
M.Andhika <48423397+andhika276@users.noreply.github.com>
2021-07-27 14:12:12 +0700

    Revert "delete dummy nilai akhir"
    This reverts commit 30a4fa4fff2a0e24f9ed09eb8784f5dc9263fa2e.

--- src/controller/Nilai_Akhir.js
+++ src/controller/Nilai_Akhir.js
@@ -87,19 +87,21 @@ export const getNilaiAkhirByMahasiswa = async (req, res, next) => {
     for(var i = 0; i<dataNilai.length; i++){
       const perkuliahan = await PerkuliahanDAO.findPerkuliahanById(dataNilai[i].id_perkuliahan)
       const matkul = await MatkulDAO.findMatkulById(perkuliahan.id_mata_kuliah)
+      var nilaiChar = ["A","AB","B","BC","C","CD","D","E"]
       const result = {
         KodeMK: matkul.id,
         MataKuliah: matkul.nama_mata_kuliah,
         SKS: matkul.sks_teori + matkul.sks_praktek,
-        Nilai: dataNilai[i].nilai_akhir
+        Nilai: nilaiChar[Math.floor(Math.random()*nilaiChar.length)] //dataNilai[i].nilai_akhir
       }
       listResult[matkul.semester - 1].totalIndeks += result.Nilai
       listResult[matkul.semester - 1].JumlahSKS += result.SKS
       listResult[matkul.semester - 1].NilaiSemester.push(result)
     }
     //Menghitung ips
+    var nilaiIP = [4.00,3.50,3.00,2.50]
     for (var j=0; j<listResult.length; j++){
-      listResult[j].IPSemester = listResult[j].totalIndeks / listResult[j].JumlahSKS
+      listResult[j].IPSemester = nilaiIP[Math.floor(Math.random()*nilaiIP.length)] // listResult[j].totalIndeks / listResult[j].JumlahSKS
     }
 
     if (dataNilai === undefined) {
