d695e662e11929d2ae1564c501f370233732f9f0
Herlana <hrlana1312@gmail.com>
2021-11-29 21:09:47 +0700

    BE1002
    - Membuat delete (delete one) data table mata kuliah

--- src/dao/Mata Kuliah.js
+++ src/dao/Mata Kuliah.js
@@ -45,3 +45,16 @@ export const insertOneMatkul = async (
     console.error(error)
   }
 }
+
+export const deleteMatkulbyId = async (Id) => {
+  try {
+    const result = await MataKuliah.destroy({
+      where: {
+        id: Id
+      }
+    })
+    return result
+  } catch (error) {
+    console.log(error)
+  }
+}
